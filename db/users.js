c3b243c82e110d9ac049c4941744f8c43b68b7b0
Nuno Gervasio <nge@hellofresh.com>
2021-05-25 12:45:09 +0200

    add db

---
+++ db/users.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    id: 1,
+    name: "aleks",
+    subreddits: ["funny"],
+    newsletter_sendout: "08:00",
+    newsletter_active: true,
+  },
+  {
+    id: 2,
+    name: "nuno",
+    subreddits: ["technology", "funny"],
+    newsletter_sendout: "08:00",
+    newsletter_active: true,
+  },
+];
+
+module.exports = { users };
